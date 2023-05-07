dc966c9349fa52200613edae417b0c7ee73d066e
ProfesorVasilev <vladigotaniq@gmail.com>
2023-05-07 15:40:19 +0300

    Add project files.

---
+++ StringReverse/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("type your string");
+string input = Console.ReadLine();
+char[] strArray = input.ToCharArray();
+Array.Reverse(strArray);
+string reversedStrng = new string (strArray);
+Console.WriteLine(reversedStrng);
